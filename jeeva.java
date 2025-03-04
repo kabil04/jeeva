8f809facf0688a76678777807e676a29f1f491b8
Kabilarajan S R <135346127+kabil04@users.noreply.github.com>
2025-03-04 10:33:19 +0530

    Add files via upload

---
+++ jeeva.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class jeevajava {
+    public static void main(String[] args) {
+     //  Scanner sc =new Scanner(System.in);
+     float arr[]={1.0,2.0,3,4,5,6,7,8,9,10};
+     int s=0,e=(arr.length-1),t=0;
+         
+         for(int i=0;i<arr.length;i++){
+            if(s<e){
+              t=arr[s];
+             arr[s]=arr[e];
+             arr[e]=t;
+               s++;
+               e--;
+            }
+            else{
+               break;
+            }
+
+         }
+         System.out.print(Arrays.toString(arr));
+
+
+
+
+
+    }
+}
+
+    
